7d79876339dc1438b4ca4007eeaeef7b65cc0942
Dario Izzo <dario.izzo@gmail.com>
2026-09-22 10:11:55 +0200

    CI brittlness improved

--- tools/gha_deploydocs.sh
+++ tools/gha_deploydocs.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 set -x
 
 conda config --set channel_priority strict
-conda install -y -q heyoka.py numpy scipy sphinx furo nbsphinx pandoc \
+conda install -y -q heyoka.py numpy scipy ipython sphinx furo nbsphinx pandoc \
     sphinx-copybutton sphinx-design
 python -m pip install --no-deps -e .
 python -m sphinx -b html doc doc/_build/html
